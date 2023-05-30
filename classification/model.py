d12438449aa9b4b4d685fad24e67b2bb966515df
cynthia <cl222@ic.ac.uk>
2023-05-30 20:14:11 +0100

    first version of dataset with small model

--- classification/model.py
+++ classification/model.py
@@ -34,20 +34,20 @@ class ResidualBlock(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, ResidualBlock, num_classes = 2):
         super(ResNet, self).__init__()
-        self.inchannel = 64
-        self.conv1 = nn.Sequential(nn.Conv2d(3, 64, kernel_size = 3, stride = 1,
+        self.inchannel = 32
+        self.conv1 = nn.Sequential(nn.Conv2d(1, 32, kernel_size = 3, stride = 1,
                                             padding = 1, bias = False), 
-                                  nn.BatchNorm2d(64), 
+                                  nn.BatchNorm2d(32), 
                                   nn.ReLU())
         
-        self.layer1 = self.make_layer(ResidualBlock, 64, 2, stride = 2)
-        self.layer2 = self.make_layer(ResidualBlock, 128, 2, stride = 2)
-        self.layer3 = self.make_layer(ResidualBlock, 256, 2, stride = 2)
-        self.layer4 = self.make_layer(ResidualBlock, 512, 2, stride = 2)
-        self.layer5 = self.make_layer(ResidualBlock, 1024, 2, stride = 2)
-        self.layer6 = self.make_layer(ResidualBlock, 2048, 2, stride = 2)
-        self.maxpool = nn.MaxPool2d(4)
-        self.fc = nn.Linear(512, num_classes)
+        self.layer1 = self.make_layer(ResidualBlock, 32, 2, stride = 2)
+        self.layer2 = self.make_layer(ResidualBlock, 64, 2, stride = 2)
+        self.layer3 = self.make_layer(ResidualBlock, 128, 2, stride = 2)
+        self.layer4 = self.make_layer(ResidualBlock, 256, 2, stride = 2)
+        # self.layer5 = self.make_layer(ResidualBlock, 1024, 2, stride = 2)
+        # self.layer6 = self.make_layer(ResidualBlock, 2048, 2, stride = 2)
+        self.maxpool = nn.MaxPool2d((1,4))
+        self.fc = nn.Linear(3584, num_classes)
     
     def make_layer(self, block, channels, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
@@ -63,8 +63,8 @@ class ResNet(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-        x = self.layer5(x)
-        x = self.layer6(x)
+        #x = self.layer5(x)
+        #x = self.layer6(x)
         x = self.maxpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
