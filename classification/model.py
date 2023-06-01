19ebe04d23d0c17fbc369e7cb9bdb3798b18fc1a
cynthia <cl222@ic.ac.uk>
2023-06-01 15:16:55 +0100

    add argument parser

--- classification/model.py
+++ classification/model.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 
+
+
 class ResidualBlock(nn.Module): 
     def __init__(self, inchannel, outchannel, stride=1):    
         super(ResidualBlock, self).__init__() 
@@ -32,22 +34,20 @@ class ResidualBlock(nn.Module):
 # define resnet
 
 class ResNet(nn.Module):
-    def __init__(self, ResidualBlock, num_classes = 24):
+    def __init__(self, ResidualBlock, args, num_classes = 24):
         super(ResNet, self).__init__()
-        self.inchannel = 64 #
-        self.conv1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size = 3, stride = 1, #
+        self.inchannel = args.foc
+        self.conv1 = nn.Sequential(nn.Conv2d(1, args.foc, kernel_size = 3, stride = 1, #
                                             padding = 1, bias = False), 
-                                  nn.BatchNorm2d(64), #
+                                  nn.BatchNorm2d(args.foc), #
                                   nn.ReLU())
         
-        self.layer1 = self.make_layer(ResidualBlock, 64, 2, stride = 2)
-        self.layer2 = self.make_layer(ResidualBlock, 128, 2, stride = 2)
-        self.layer3 = self.make_layer(ResidualBlock, 256, 2, stride = 2)
-        self.layer4 = self.make_layer(ResidualBlock, 512, 2, stride = 2)
-        # self.layer5 = self.make_layer(ResidualBlock, 1024, 2, stride = 2)
-        # self.layer6 = self.make_layer(ResidualBlock, 2048, 2, stride = 2)
+        self.layer1 = self.make_layer(ResidualBlock, args.foc*2, 2, stride = 2)
+        self.layer2 = self.make_layer(ResidualBlock, args.foc*4, 2, stride = 2)
+        self.layer3 = self.make_layer(ResidualBlock, args.foc*8, 2, stride = 2)
+        self.layer4 = self.make_layer(ResidualBlock, args.foc*16, 2, stride = 2)
         self.maxpool = nn.MaxPool2d((1,4))
-        self.fc = nn.Linear(3584*4, num_classes)
+        self.fc = nn.Linear(3584*4, num_classes) 
     
     def make_layer(self, block, channels, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
@@ -63,14 +63,12 @@ class ResNet(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-        #x = self.layer5(x)
-        #x = self.layer6(x)
         x = self.maxpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
     
 # please do not change the name of this class
-def MyResNet():
-    return ResNet(ResidualBlock)
+def MyResNet(args):
+    return ResNet(ResidualBlock, args=args)
 
--- classification/training.py
+++ classification/training.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
+import argparse
 from model import MyResNet
 from torch.utils.data import DataLoader, Dataset, random_split
 
@@ -14,14 +15,29 @@ else:
     device = torch.device('cpu')
 print(device)
     
-print_every = 10
-def check_accuracy(loader, model, analysis=False):
+def get_args():
+    parser = argparse.ArgumentParser(description='ResNet18')
+    parser.add_argument('--lr', type=float, default=0.001,
+                        help='learning rate [default: 0.001]')
+    parser.add_argument('--wd', type=float, default=0.0,
+                        help='weight decay [default: 0.0]')
+    parser.add_argument('--foc', type=int, default=64,
+                        help='out-channel of first Conv2D in ResNet18 [default: 64]')
+    parser.add_argument('--batch', type=int, default=16,
+                        help='batch size [default: 16]')
+    args = parser.parse_args()
+    return args
+    
+
+print_every = 50
+def check_accuracy(loader, model, analysis=False, logging=True, logfile=None):
     # function for test accuracy on validation and test set
     num_correct = 0
     num_samples = 0
     model.eval()  # set model to evaluation mode
     with torch.no_grad():
         for t, (x, y) in enumerate(loader):
+            x = x[:, None, :, :] # FIX THIS LATER
             x = x.to(device=device, dtype=dtype)  # move to device
             y = y.to(device=device, dtype=torch.long)
             scores = model(x)
@@ -36,9 +52,11 @@ def check_accuracy(loader, model, analysis=False):
               stack_predicts = torch.cat([stack_predicts, preds], 0)
         acc = float(num_correct) / num_samples
         print('Got %d / %d correct of val set (%.2f)' % (num_correct, num_samples, 100 * acc))
+        if logging:
+            logfile.write('Got %d / %d correct of val set (%.2f) \n' % (num_correct, num_samples, 100 * acc))
         return float(acc)
 
-def train_part(model, optimizer, epochs=1):
+def train_part(model, optimizer, epochs=1, logging=True, logfile=None):
     """
     Train a model on NaturalImageNet using the PyTorch Module API.
     
@@ -50,9 +68,11 @@ def train_part(model, optimizer, epochs=1):
     Returns: Nothing, but prints model accuracies during training.
     """
     model = model.to(device=device)  # move the model parameters to CPU/GPU
+
     for e in range(epochs):
         for t, (x, y) in enumerate(loader_train):
             model.train()  # put model to training mode
+            x = x[:, None, :, :] # FIX THIS LATER
             x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
             y = y.to(device=device, dtype=torch.long)
 
@@ -68,27 +88,41 @@ def train_part(model, optimizer, epochs=1):
 
             if t % print_every == 0:
                 print('Epoch: %d, Iteration %d, loss = %.4f' % (e, t, loss.item()))
-        check_accuracy(loader_val, model)
+            if logging:
+                logfile.write('Epoch: %d, Iteration %d, loss = %.4f \n' % (e, t, loss.item()))
+        check_accuracy(loader_val, model, logging=logging, logfile=logfile)
                 
 if __name__== "__main__":
 
     # load data
-    train_ds = torch.load("train_data.pt")
-    val_ds = torch.load("val_data.pt")
-
+    print("before data loaded")
+    train_ds = torch.load("train_data24.pt")
+    val_ds = torch.load("val_data24.pt")
+    print("data loaded")
     # define dataloader
-    loader_train = torch.utils.data.DataLoader(train_ds, batch_size=16, shuffle=True)
-    loader_val = torch.utils.data.DataLoader(val_ds, batch_size=16, shuffle=True)
+    loader_train = DataLoader(train_ds, batch_size=args.batch, shuffle=True)
+    loader_val = DataLoader(val_ds, batch_size=args.batch, shuffle=True)
     
     print(next(iter(loader_train)))
     # define and train the network
     model = MyResNet()
-    optimizer = torch.optim.Adamax(model.parameters(), lr=0.001, weight_decay=1e-7) 
+    # print(model)
+    optimizer = torch.optim.Adamax(model.parameters(), lr=args.lr, weight_decay=args.wd) 
 
     params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print("Total number of parameters is: {}".format(params))
+    print("Total number of parameters is: {} \n".format(params))
 
-    # train_part(model, optimizer, epochs = 1)
+    logging = True
+    if logging:
+        logfile_name = "logfile24.txt"
+        logfile = open(logfile_name, "w")
+        logfile.write("Total number of model parameters is: {} \n".format(params))
+        logfile.write("Training data size is: {} \n".format(len(train_ds)))
+    else:
+        logfile = None
+
+    train_part(model, optimizer, epochs = 20, logging=logging, logfile=logfile)
+    logfile.close()
 
     # # report test set accuracy
     # check_accuracy(loader_val, model, analysis=False)
