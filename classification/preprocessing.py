30f841c7840933b8a281823db34b4233a3ffff84
cynthia <cl222@ic.ac.uk>
2023-06-16 06:53:25 +0100

    preprocessing used to preprocess unbiased, biased, neutral train and test 001

--- classification/preprocessing.py
+++ classification/preprocessing.py
@@ -6,55 +6,50 @@ import pandas as pd
 from torch.utils.data import DataLoader, Dataset, random_split, TensorDataset
 from os import listdir
 from os.path import isfile, join
+from joblib import Parallel, delayed
 
-def load_data(datapath, metadata_path, transform=torchaudio.transforms.MelSpectrogram(sample_rate=44100, n_mels=16)):
-    df = pd.read_csv(metadata_path, sep='\t')
-    features = torch.empty(size=[1, 16, 887])
-    labels = torch.empty(size=[1])
-    for idx in range(len(df)):
-        audio_file = datapath + df.loc[idx, 'filename']
-        label = df.loc[idx, 'label']
-        waveform, sample_rate = torchaudio.load(audio_file)
-        specgram = transform(waveform)
-        features = torch.cat((features, specgram), dim=0)
-        labels = torch.cat((labels, torch.tensor([label])), dim=0)
-    features = features[1:,:,:] # remove first empty tensor
-    labels = labels[1:] # remove first empty tensor
+def load_data(datapath, metadata_path, use="train", transform=torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_mels=32)):
+    metadata_df = pd.read_csv(metadata_path, sep=',')
+    output = Parallel(n_jobs=1)(delayed(data_transform)(metadata_df.iloc(0)[i], j) for (i, j) in zip(range(len(metadata_df)), [datapath]*len(metadata_df)))
+    features = torch.stack([tensor_tuple[0] for tensor_tuple in output])
+    labels = torch.stack([tensor_tuple[1] for tensor_tuple in output])
     data = TensorDataset(features, labels)
     return data
 
-def write_metadata(data_path):
-    files = sorted([f for f in listdir(data_path) if isfile(join(data_path, f))])
-    labels = [int(name[0].islower()) for name in train_files]
+def data_transform(one_metadata,datapath):
+    df = one_metadata
+    d = {"major":1, "minor":0}
+#    datapath = "/rds/general/user/cl222/home/audio/unbiased_train/"
+    audio_file = datapath + one_metadata['filename']+ ".wav"
+    label = one_metadata['scale']
+    waveform, sample_rate = torchaudio.load(audio_file)
+    transform=torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_mels=32)
+    specgram = transform(waveform)
+    return (specgram, torch.tensor([d[label]]))
 
-    df = pd.DataFrame({'filename': files,
-                    'label': labels})
-
-    saved_path = "/rds/general/user/cl222/home/audio/metadata" ## CHANGE ME
-    df.to_csv(saved_path, sep='\t', index=False)
-    return None
-    
+# def data_transform_test(one_metadata):
+#     df = one_metadata
+#     d = {"major":1, "minor":0}
+#     datapath = "/rds/general/user/cl222/home/audio/unbiased_test/"
+#     audio_file = datapath + one_metadata['filename']+ ".wav"
+#     label = one_metadata['scale']
+#     waveform, sample_rate = torchaudio.load(audio_file)
+#     transform=torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_mels=32)
+#     specgram = transform(waveform)
+#     return (specgram, torch.tensor([d[label]]))
 
 if __name__=="__main__":
 
-    # Local path
-    #metadata_train = "/Users/chia-hsinlin/Documents/Imperial/individual_project/msc_project/tempsnd/metadata_train.csv"
-    #train_path = "/Users/chia-hsinlin/Documents/Imperial/individual_project/msc_project/tempsnd/train_data/"
+    train_path = "/rds/general/user/cl222/home/audio/unbiased_train/"
+    metadata_train = "/rds/general/user/cl222/home/audio/metadata_unbiased_train.csv"
 
-    #metadata_test = "/Users/chia-hsinlin/Documents/Imperial/individual_project/msc_project/tempsnd/metadata_test.csv"
-    #test_path = "/Users/chia-hsinlin/Documents/Imperial/individual_project/msc_project/tempsnd/test_data/"
-
-    # School path
-    train_path = "/rds/general/user/cl222/home/audio/train_data/"
-    metadata_train = "/rds/general/user/cl222/home/audio/metadata_train.csv"
-
-    test_path = "/rds/general/user/cl222/home/audio/test_data/"
-    metadata_test = "/rds/general/user/cl222/home/audio/metadata_test.csv"
+    test_path = "/rds/general/user/cl222/home/audio/unbiased_test/"
+    metadata_test = "/rds/general/user/cl222/home/audio/metadata_unbiased_test.csv"
 
 
-    train_data = load_data(train_path, metadata_train)
+    train_data = load_data(train_path, metadata_train, use="train")
     print("finish loading train data")
-    test_data = load_data(test_path, metadata_test)
+    test_data = load_data(test_path, metadata_test, use="test")
     print("finish loading test data")
 
     # Random split of 80:20 between training and validation
@@ -67,36 +62,15 @@ if __name__=="__main__":
     # save data
     save_data = True
     if save_data:
-        torch.save(train_ds, "train_data.pt")
-        torch.save(val_ds, "val_data.pt")
-        torch.save(test_data, "test_data.pt")
+        torch.save(train_ds, "/rds/general/user/cl222/home/msc_project/classification/unbiased_train_data.pt")
+        torch.save(val_ds, "/rds/general/user/cl222/home/msc_project/classification/unbiased_val_data.pt")
+        torch.save(test_data, "/rds/general/user/cl222/home/msc_project/classification/unbiased_test_data.pt")
         print("finish saving data")
 
-    # train_data = torch.load("train_data.pt")
-    # val_data = torch.load("val_data.pt")
-    # test_data = torch.load("test_data.pt")
-    # print(len(train_data))
-    # print(len(val_data))
-    # print(len(test_data))
-    # print(test_data[0])
+    train_data = torch.load("/rds/general/user/cl222/home/msc_project/classification/unbiased_train_data.pt")
+    val_data = torch.load("/rds/general/user/cl222/home/msc_project/classification/unbiased_val_data.pt")
+    test_data = torch.load("/rds/general/user/cl222/home/msc_project/classification/unbiased_test_data.pt")
+    print(len(train_data))
+    print(len(val_data))
+    print(len(test_data))
     
-
-
-# class SoundDS(Dataset):
-#     def __init__(self, df, data_path, transform=torchaudio.transforms.MelSpectrogram(sample_rate=44100, n_mels=16)):
-#         self.df = df
-#         self.data_path = str(data_path)
-#         self.duration = 4000
-#         self.sr = 44100
-#         self.channel = 1
-#         self.transform = transform
-        
-#     def __len__(self):
-#         return len(self.df) 
-    
-#     def __getitem__(self, idx):
-#         audio_file = self.data_path + self.df.loc[idx, 'filename']
-#         label = self.df.loc[idx, 'label']
-#         waveform, sample_rate = torchaudio.load(audio_file)
-#         specgram = self.transform(waveform)
-#         return (specgram, label)
