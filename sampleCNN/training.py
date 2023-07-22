cb69109955585e254e03a11ec3e46739c6059757
cynthia <cl222@ic.ac.uk>
2023-07-22 08:12:05 +0100

    training script to get confusion matrix

--- sampleCNN/training.py
+++ sampleCNN/training.py
@@ -3,11 +3,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
+import numpy as np
 import argparse
 import os
 from sampleCNN import MySampleCNN
+from get_data import get_dataset
 import wandb
-from torch.utils.data import DataLoader, Dataset, random_split, TensorDataset
+from torch.utils.data import DataLoader, Dataset, Subset, random_split, TensorDataset
 print("package imported ")
 
 USE_GPU = True
@@ -20,6 +22,19 @@ else:
 print(device)
 torch.cuda.manual_seed(5)
 
+def get_data_args(shift_type=None, shift_strength=0.0, shift_way=None, waveshape=None):
+    parser = argparse.ArgumentParser(description='data_args')
+    parser.add_argument('--shift_type', type=str, default=shift_type,
+                        help='type of dataset shift [default: None]')
+    parser.add_argument('--shift_strength', type=float, default=shift_strength,
+                        help='strength of dataset shift [default: 0.0]')
+    parser.add_argument('--shift_way', type=list, default=shift_way,
+                        help='way to construct shift. [default: None]')
+    parser.add_argument('--waveshape', type=str, default=waveshape,
+                        help='waveshape made up of the data if no shift. [default: None]')
+    args = parser.parse_args()
+    return args
+    
 def get_training_args():
     parser = argparse.ArgumentParser(description='training_args')
     parser.add_argument('--lr', type=float, default=0.0001,
@@ -27,13 +42,15 @@ def get_training_args():
     parser.add_argument('--wd', type=float, default=0.0,
                         help='weight decay [default: 0.0]')
     parser.add_argument('--batch', type=int, default=64,
-                        help='batch size [default: 16]')
+                        help='batch size [default: 64]')
     parser.add_argument("--epochs", type=int, default=8,
                         help="number of epoch for training")
     parser.add_argument("--optimizer", type=str, default="Adam",
                         help="optimizer used in training")
     parser.add_argument("--num_classes", type=int, default=2,
                         help="number of class labels [default: 2]")
+    parser.add_argument("--clip_grad_norm", type=float, default=1.0,
+                        help="maximum magnitude of gradient updated [default: 2]")
     args = parser.parse_args()
     return args
 
@@ -47,24 +64,29 @@ def get_model_args():
     return args
 
 def load_data():
-        unbiased_train_ds = torch.load("unbiased_train_wav.pt") 
-        unbiased_val_ds = torch.load("unbiased_val_wav.pt")
-        unbiased_test_ds = torch.load("unbiased_test_wav.pt")
-        biased_train_ds = torch.load("biased_train_wav.pt")
-        biased_val_ds = torch.load("biased_val_wav.pt")
-        anti_biased_test_ds = torch.load("anti-biased_test_wav.pt")
-        neutral_test_ds = torch.load("neutral_test_wav.pt")
-        square_test_ds = torch.load("square_test_wav.pt")
-        triangle_test_ds = torch.load("triangle_test_wav.pt")
-        return [unbiased_train_ds, unbiased_val_ds, unbiased_test_ds, biased_train_ds, biased_val_ds, anti_biased_test_ds, neutral_test_ds, square_test_ds, triangle_test_ds]
+        sine_train_ds = torch.load("sine_train_data.pt") 
+        sine_val_ds = torch.load("sine_val_data.pt")
+        sine_test_ds = torch.load("sine_test_data.pt")
+        square_train_ds = torch.load("square_train_data.pt") 
+        square_val_ds = torch.load("square_val_data.pt")
+        square_test_ds = torch.load("square_test_data.pt")
+        triangle_train_ds = torch.load("triangle_train_data.pt") 
+        triangle_val_ds = torch.load("triangle_val_data.pt")
+        triangle_test_ds = torch.load("triangle_test_data.pt")
+        sawtooth_train_ds = torch.load("sawtooth_train_data.pt") 
+        sawtooth_val_ds = torch.load("sawtooth_val_data.pt")
+        sawtooth_test_ds = torch.load("sawtooth_test_data.pt")
+        return [sine_train_ds, sine_val_ds, sine_test_ds, square_train_ds, square_val_ds, square_test_ds, \
+            triangle_train_ds, triangle_val_ds, triangle_test_ds, sawtooth_train_ds, sawtooth_val_ds, sawtooth_test_ds]
 
 def load_and_log_data():
     with wandb.init(project="msc_project", job_type="load-data") as run:
         datasets = load_data()
-        names = ["unbiased_train", "unbiased_val", "unbiased_test", "biased_train", "biased_val", "anti-biased_test", "neutral_test", "square_test", "triangle_test"]
+        names = ["sine_train_ds", "sine_val_ds", "sine_test_ds", "square_train_ds", "square_val_ds", "square_test_ds", \
+            "triangle_train_ds", "triangle_val_ds", "triangle_test_ds", "sawtooth_train_ds", "sawtooth_val_ds", "sawtooth_test_ds"]
         raw_data = wandb.Artifact(
             name="wav_dataset_exp001", type="dataset",
-            description="waveform dataset used to run experiment 001",
+            description="dataset used to run experiment 001",
             metadata={"source": "generated by parallel_data_generation 001",
                       "sizes":[len(dataset) for dataset in datasets]})
         print("finish create artifact")
@@ -108,33 +130,36 @@ def train(model, train_loader, val_loader, args):
         for t, (x, y) in enumerate(train_loader):
             x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
             y = y.to(device=device, dtype=dtype) if args.num_classes==2 else y.to(device=device, dtype=torch.long) 
-            scores = torch.squeeze(model(x), dim=-1) # since model is full-conv
+            scores = torch.squeeze(model(x)) # since model is full-conv
             loss = loss_fcn(scores, y)
             optimizer.zero_grad()
             loss.backward()
             # Update the parameters of the model using the gradients
+            torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip_grad_norm)
             optimizer.step()
             example_ct += len(x)
             if t % print_every == 0:
                 print('Epoch: %d, Iteration %d, loss = %.4f' % (e, t, loss.item()))
                 train_log(loss, example_ct, e)
-        loss, accuracy = test(model, val_loader, args)
+        loss, accuracy, outputs = test(model, val_loader, args)
         test_log(loss, accuracy, example_ct, e)
 
-def train_and_log(train_args, model_args):
+def train_and_log(train_args, model_args, data_args):
     with wandb.init(project="msc_project", job_type="train", config=train_args) as run:
         config = wandb.config
         data = run.use_artifact('wav_dataset_exp001:latest')
         data_dir = data.download()
         data_dir = data_dir[2:]
-        unbiased_train_ds = torch.load(os.path.join(data_dir+"/biased_train.pt"))
-        unbiased_val_ds = torch.load(os.path.join(data_dir+"/biased_val.pt"))
-        #biased_train_ds = torch.load(os.path.join(data_dir+"/biased_train.pt"))
-        #biased_val_ds = torch.load(os.path.join(data_dir+"/biased_val.pt"))
+        train_ds = get_dataset(data_dir, "train", shift_type=data_args.shift_type, waveshape=data_args.waveshape,\
+                    shift_strength=data_args.shift_strength, shift_way=data_args.shift_way)
+        val_ds = get_dataset(data_dir, "val", shift_type=data_args.shift_type, waveshape=data_args.waveshape,\
+                    shift_strength=data_args.shift_strength, shift_way=data_args.shift_way)
+        data_config = vars(data_args)
+        config.update(data_config)
         print("finish load data from artifact")
 
-        train_loader = DataLoader(unbiased_train_ds, batch_size=train_args.batch, shuffle=True)
-        val_loader = DataLoader(unbiased_val_ds, batch_size=train_args.batch, shuffle=True)
+        train_loader = DataLoader(train_ds, batch_size=train_args.batch, shuffle=True)
+        val_loader = DataLoader(val_ds, batch_size=train_args.batch, shuffle=True)
         print("finish initialise data loader")
 
         model_artifact = run.use_artifact("SampleCNN:latest")
@@ -166,6 +191,7 @@ def test(model, test_loader, args):
     test_loss = 0
     num_correct = 0
     num_samples = 0
+    outputs = None
     if args.num_classes==2:
         loss_fcn = nn.BCELoss(reduction='sum')
     else:
@@ -174,8 +200,11 @@ def test(model, test_loader, args):
         for t, (x, y) in enumerate(test_loader):
             x = x.to(device=device, dtype=dtype)  # move to device
             y = y.to(device=device, dtype=dtype) if args.num_classes==2 else y.to(device=device, dtype=torch.long) 
-            scores = torch.squeeze(model(x), dim=-1)
-            # test_loss += loss_fcn(scores, y) # reduction=sum?
+            scores = torch.squeeze(model(x))
+            if outputs is None:
+                outputs = scores
+            else:
+                outputs = torch.cat((outputs, scores))
             if args.num_classes==2:
                 test_loss += loss_fcn(scores, y)
                 preds = scores>0.5
@@ -189,63 +218,26 @@ def test(model, test_loader, args):
             print("num_correct:", num_correct, "num_samples:", num_samples)
     test_loss /= len(test_loader.dataset)
     acc = float(num_correct) / num_samples
-    return test_loss, float(acc)
-
-def get_hardest_k_examples(model, testing_set, args, k=32):
-    model.eval()
-    loader = DataLoader(testing_set, 1, shuffle=False)
-    # get the losses and predictions for each item in the dataset
-    losses = None
-    predictions = None
-    if args.num_classes==2:
-        loss_fcn = nn.BCELoss()
-    else:
-        loss_fcn = F.cross_entropy
-    with torch.no_grad():
-        for data, target in loader:
-            data = data.to(device=device, dtype=dtype) 
-            target = target.to(device=device, dtype=dtype) if args.num_classes==2 else y.to(device=device, dtype=torch.long) 
-            output = torch.squeeze(model(data), dim=-1) 
-            loss = loss_fcn(output, target)
-            pred = output.argmax(dim=1, keepdim=True)
-            if losses is None:
-                losses = loss.view((1, 1))
-                predictions = pred
-            else:
-                losses = torch.cat((losses, loss.view((1, 1))), 0)
-                predictions = torch.cat((predictions, pred), 0)
-    argsort_loss = torch.argsort(losses, dim=0)
-
-    argsort_loss = argsort_loss.cpu()
-    losses = losses.cpu()
-    predictions = predictions.cpu()
-    highest_k_losses = losses[argsort_loss[-k:]]
-    hardest_k_examples = testing_set[argsort_loss[-k:]][0]
-    true_labels = testing_set[argsort_loss[-k:]][1]
-    predicted_labels = predictions[argsort_loss[-k:]]
-    return highest_k_losses, hardest_k_examples, true_labels, predicted_labels
+    return test_loss, float(acc), outputs
 
 def evaluate(model, test_loader, args):
-    loss, accuracy = test(model, test_loader, args)
-    highest_losses, hardest_examples, true_labels, predictions = get_hardest_k_examples(model, test_loader.dataset, args)
-    return loss, accuracy, highest_losses, hardest_examples, true_labels, predictions
+    loss, accuracy, outputs = test(model, test_loader, args)
+    return loss, accuracy, outputs
 
 def evaluate_and_log(wav_data_path, args=None):
     with wandb.init(project="msc_project", job_type="report", config=args) as run:
         data = run.use_artifact('wav_dataset_exp001:latest')
         data_dir = data.download()
         data_dir = data_dir[2:]
-        unbiased_test = torch.load(os.path.join(data_dir+"/unbiased_test.pt"))
-        anti_biased_test = torch.load(os.path.join(data_dir+"/anti-biased_test.pt"))
-        neutral_test = torch.load(os.path.join(data_dir+"/neutral_test.pt"))
-        square_test = torch.load(os.path.join(data_dir+"/square_test.pt"))
-        triangle_test = torch.load(os.path.join(data_dir+"/triangle_test.pt"))
+        sine_test_data = get_dataset(data_dir, "test", waveshape="sine")
+        square_test_data = get_dataset(data_dir, "test", waveshape="square")
+        sawtooth_test_data = get_dataset(data_dir, "test", waveshape="sawtooth")
+        triangle_test_data = get_dataset(data_dir, "test", waveshape="triangle")
 
-        unbiased_test_loader = DataLoader(unbiased_test, shuffle=False, batch_size=128)
-        anti_biased_test_loader = DataLoader(anti_biased_test, shuffle=False, batch_size=128)
-        neutral_test_loader = DataLoader(neutral_test, shuffle=False, batch_size=128)
-        square_test_loader = DataLoader(square_test, shuffle=False, batch_size=128)
-        triangle_test_loader = DataLoader(triangle_test, shuffle=False, batch_size=128)
+        sine_test_loader = DataLoader(sine_test_data, shuffle=False, batch_size=64)
+        sawtooth_test_loader = DataLoader(sawtooth_test_data, shuffle=False, batch_size=64)
+        square_test_loader = DataLoader(square_test_data, shuffle=False, batch_size=64)
+        triangle_test_loader = DataLoader(triangle_test_data, shuffle=False, batch_size=64)
 
         model_artifact = run.use_artifact("trained-sampleCNN:latest")
         model_dir = model_artifact.download()
@@ -257,42 +249,28 @@ def evaluate_and_log(wav_data_path, args=None):
         model.load_state_dict(torch.load(model_path))
         model.to(device)
 
-        wav_unbiased_test_path = "/rds/general/user/cl222/home/audio/unbiased_train"
-        wav_anti_biased_test_path = "/rds/general/user/cl222/home/audio/anti-biased_test"
-        wav_neutral_test_path = "/rds/general/user/cl222/home/audio/neutral_test"
+        sine_loss, sine_accuracy, sine_outputs = evaluate(model, sine_test_loader, args)
+        sawtooth_loss, sawtooth_accuracy, sawtooth_outputs = evaluate(model, sawtooth_test_loader, args)
+        triangle_loss, triangle_accuracy, triangle_outputs = evaluate(model, triangle_test_loader, args)
+        square_loss, square_accuracy, square_outputs = evaluate(model, square_test_loader, args)
 
-        # test on unbaised, anti-biased, and neutral data
-        print("start evaluating unbiased test set")
-        unbiased_loss, unbiased_accuracy, unbiased_highest_losses, unbiased_hardest_examples, \
-            unbiased_true_labels, unbiased_preds = evaluate(model, unbiased_test_loader, args)
-        print("start evaluating anti-biased test set")
-        anti_biased_loss, anti_biased_accuracy, anti_biased_highest_losses, anti_biased_hardest_examples, \
-            anti_biased_true_labels, anti_biased_preds = evaluate(model, anti_biased_test_loader, args)
-        print("start evaluating neutral test set")
-        neutral_loss, neutral_accuracy, neutral_highest_losses, neutral_hardest_examples, \
-            neutral_true_labels, neutral_preds = evaluate(model, neutral_test_loader, args)
-        print("start evaluating square test set")
-        square_loss, square_accuracy, square_highest_losses, square_hardest_examples, \
-            square_true_labels, square_preds = evaluate(model, square_test_loader, args)
-        print("start evaluating triangle test set")
-        triangle_loss, triangle_accuracy, triangle_highest_losses, triangle_hardest_examples, \
-            triangle_true_labels, triangle_preds = evaluate(model, triangle_test_loader, args)        
+        sine_outputs = sine_outputs.numpy(force=True)
+        sawtooth_outputs = sawtooth_outputs.numpy(force=True)
+        triangle_outputs = triangle_outputs.numpy(force=True)
+        square_outputs = square_outputs.numpy(force=True)      
 
-        run.summary.update({"unbiased_loss": unbiased_loss, "unbiased_accuracy": unbiased_accuracy,
-                            "anti_biased_loss":anti_biased_loss, "anti_biased_accuracy":anti_biased_accuracy,
-                            "neutral_loss":neutral_loss, "neutral_accuracy":neutral_accuracy,
+        np.savetxt('sine_outputs.txt', sine_outputs, fmt='%1.3f')
+        np.savetxt('sawtooth_outputs.txt', sawtooth_outputs, fmt='%1.3f')
+        np.savetxt('triangle_outputs.txt', triangle_outputs, fmt='%1.3f')
+        np.savetxt('square_outputs.txt', square_outputs, fmt='%1.3f')
+        run.summary.update({"sine_loss": sine_loss, "sine_accuracy": sine_accuracy,
+                            "sawtooth_loss":sawtooth_loss, "sawtooth_accuracy":sawtooth_accuracy,
                             "square_loss":square_loss, "square_accuracy":square_accuracy,
                             "triangle_loss": triangle_loss, "triangle_accuracy":triangle_accuracy})
-
-        # wandb.log({"unbiased_high-loss-examples":
-        #     [wandb.Audio(torch.squeeze(hard_example, dim=0).cpu().detach().numpy(), sample_rate=16000, caption=str(int(pred)) + "," +  str(int(label)))
-        #      for hard_example, pred, label in zip(unbiased_hardest_examples, unbiased_preds, unbiased_true_labels)],
-        #      "anti_biased_high-loss-examples":
-        #      [wandb.Audio(torch.squeeze(hard_example,dim=0).cpu().detach().numpy(), sample_rate=16000, caption=str(int(pred)) + "," +  str(int(label)))
-        #      for hard_example, pred, label in zip(anti_biased_hardest_examples, anti_biased_preds, anti_biased_true_labels)],
-        #      "neutral_high-loss-examples":
-        #      [wandb.Audio(torch.squeeze(hard_example,dim=0).cpu().detach().numpy(), sample_rate=16000, caption=str(int(pred)) + "," +  str(int(label)))
-        #      for hard_example, pred, label in zip(neutral_hardest_examples, neutral_preds, neutral_true_labels)]})
+        wandb.save("sine_outputs.txt")
+        wandb.save("sawtooth_outputs.txt")
+        wandb.save("triangle_outputs.txt")
+        wandb.save("square_outputs.txt")
     return None
 
 def train_log(loss, example_ct, epoch):
@@ -312,5 +290,8 @@ if __name__== "__main__":
     model_args = get_model_args()
     # load_and_log_data() # use if have new data
     # build_and_log_model(model_args) # use if have new model
-    # train_and_log(train_args, model_args)
-    evaluate_and_log(model_args)
+    waveshapes = ["sine", "square", "triangle", "sawtooth"]
+    for waveshape in waveshapes:
+        train_data_args = get_data_args(shift_type=None, shift_strength=0.0, shift_way=None, waveshape=waveshape)
+        train_and_log(train_args, model_args, train_data_args)
+        evaluate_and_log(model_args)
