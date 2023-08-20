146c5d1904ba443e765edb7d7e7d14dbbdf77051
cynthia <cl222@ic.ac.uk>
2023-08-20 08:58:17 +0100

    add del statement for memory issues

--- sampleCNN/wave_preprocessing.py
+++ sampleCNN/wave_preprocessing.py
@@ -4,7 +4,7 @@ import torchaudio
 import matplotlib.pyplot as plt
 # import librosa
 import pandas as pd
-from torch.utils.data import DataLoader, Dataset, random_split, TensorDataset
+from torch.utils.data import DataLoader, Dataset, random_split, TensorDataset, Subset
 import os, sys
 from os import listdir
 from os.path import isfile, join
@@ -45,72 +45,90 @@ def preprocess_data(data_loader):
     return data
 
 if __name__=="__main__":
+    num_cpu = 200
 
     sine_train_data = CynthiaDataset(metadata_sine_train, sine_train_path)
-    square_train_data = CynthiaDataset(metadata_square_train, square_train_path)
-    sawtooth_train_data = CynthiaDataset(metadata_sawtooth_train, sawtooth_train_path)
-    triangle_train_data = CynthiaDataset(metadata_triangle_train, triangle_train_path)
+    # square_train_data = CynthiaDataset(metadata_square_train, square_train_path)
+    # sawtooth_train_data = CynthiaDataset(metadata_sawtooth_train, sawtooth_train_path)
+    # triangle_train_data = CynthiaDataset(metadata_triangle_train, triangle_train_path)
 
     # # train-val split
     train_len = len(square_train_data)
     num_train = round(train_len * 0.8)
     num_val = train_len - num_train
-    sine_train_ds, sine_val_ds = random_split(sine_train_data, [num_train, num_val], torch.Generator().manual_seed(42))
-    square_train_ds, square_val_ds = random_split(square_train_data, [num_train, num_val], torch.Generator().manual_seed(42))
-    sawtooth_train_ds, sawtooth_val_ds = random_split(sawtooth_train_data, [num_train, num_val], torch.Generator().manual_seed(42))
-    triangle_train_ds, triangle_val_ds = random_split(triangle_train_data, [num_train, num_val], torch.Generator().manual_seed(42))
 
-    sine_train_loader = DataLoader(sine_train_ds, batch_size=len(sine_train_ds)//200, num_workers=200)
-    sine_val_loader = DataLoader(sine_val_ds, batch_size=len(sine_val_ds)//200, num_workers=200)
-    square_train_loader = DataLoader(square_train_ds, batch_size=len(square_train_ds)//200, num_workers=200)
-    square_val_loader = DataLoader(square_val_ds, batch_size=len(square_val_ds)//200, num_workers=200)
-    sawtooth_train_loader = DataLoader(sawtooth_train_ds, batch_size=len(sawtooth_train_ds)//200, num_workers=200)
-    sawtooth_val_loader = DataLoader(sawtooth_val_ds, batch_size=len(sawtooth_val_ds)//200, num_workers=200)
-    triangle_train_loader = DataLoader(triangle_train_ds, batch_size=len(triangle_train_ds)//200, num_workers=200)
-    triangle_val_loader = DataLoader(triangle_val_ds, batch_size=len(triangle_val_ds)//200, num_workers=200)
+    # 1 to 25000 are majors, 25001 to 50000 are miniors
+    # Let 1 to 20000 and 25001 to 45000 be in training set. Total = 40000
+    # Let 20001 to 25000 and 45001 to 50000 in val set. Total = 10000
+    train_indices = list(range(0, 20000)) + list(range(25000, 45000))
+    val_indices = list(range(20000, 25000)) + list(range(45000, 50000))
+
+    sine_train_ds, sine_val_ds = Subset(sine_train_data, train_indices), Subset(sine_train_data, val_indices)
+    square_train_ds, square_val_ds =  Subset(square_train_data, train_indices), Subset(square_train_data, val_indices)
+    sawtooth_train_ds, sawtooth_val_ds = Subset(sawtooth_train_data, train_indices), Subset(sawtooth_train_data, val_indices)
+    triangle_train_ds, triangle_val_ds = Subset(triangle_train_data, train_indices), Subset(triangle_train_data, val_indices)
+
+    sine_train_loader = DataLoader(sine_train_ds, batch_size=len(sine_train_ds)//num_cpu, num_workers=num_cpu)
+    sine_val_loader = DataLoader(sine_val_ds, batch_size=len(sine_val_ds)//num_cpu, num_workers=num_cpu)
+    # square_train_loader = DataLoader(square_train_ds, batch_size=len(square_train_ds)//num_cpu, num_workers=num_cpu)
+    # square_val_loader = DataLoader(square_val_ds, batch_size=len(square_val_ds)//num_cpu, num_workers=num_cpu)
+    # sawtooth_train_loader = DataLoader(sawtooth_train_ds, batch_size=len(sawtooth_train_ds)//num_cpu, num_workers=num_cpu)
+    # sawtooth_val_loader = DataLoader(sawtooth_val_ds, batch_size=len(sawtooth_val_ds)//num_cpu, num_workers=num_cpu)
+    # triangle_train_loader = DataLoader(triangle_train_ds, batch_size=len(triangle_train_ds)//num_cpu, num_workers=num_cpu)
+    # triangle_val_loader = DataLoader(triangle_val_ds, batch_size=len(triangle_val_ds)//num_cpu, num_workers=num_cpu)
 
     sine_train_ds = preprocess_data(sine_train_loader)
+    torch.save(sine_train_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/sine_train24_data.pt")
+    del sine_train_ds
     sine_val_ds = preprocess_data(sine_val_loader)
-    square_train_ds = preprocess_data(square_train_loader)
-    square_val_ds = preprocess_data(square_val_loader)
-    sawtooth_train_ds = preprocess_data(sawtooth_train_loader)
-    sawtooth_val_ds = preprocess_data(sawtooth_val_loader)
-    triangle_train_ds = preprocess_data(triangle_train_loader)
-    triangle_val_ds = preprocess_data(triangle_val_loader)
-    print("finish preprocess train/val data")
-
-    torch.save(sine_train_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/sine_train_data.pt")
-    torch.save(sine_val_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/sine_val_data.pt")
-    torch.save(square_train_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/square_train_data.pt")
-    torch.save(square_val_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/square_val_data.pt")
-    torch.save(sawtooth_train_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/sawtooth_train_data.pt")
-    torch.save(sawtooth_val_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/sawtooth_val_data.pt")
-    torch.save(triangle_train_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/triangle_train_data.pt")
-    torch.save(triangle_val_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/triangle_val_data.pt")
+    torch.save(sine_val_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/sine_val24_data.pt")
+    del sine_val_ds
+    # square_train_ds = preprocess_data(square_train_loader)
+    # torch.save(square_train_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/square_train_data.pt")
+    # del square_train_ds
+    # square_val_ds = preprocess_data(square_val_loader)
+    # torch.save(square_val_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/square_val_data.pt")
+    # del square_val_ds
+    # sawtooth_train_ds = preprocess_data(sawtooth_train_loader)
+    # torch.save(sawtooth_train_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/sawtooth_train_data.pt")
+    # del sawtooth_train_ds
+    # sawtooth_val_ds = preprocess_data(sawtooth_val_loader)
+    # torch.save(sawtooth_val_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/sawtooth_val_data.pt")
+    # del sawtooth_val_ds
+    # triangle_train_ds = preprocess_data(triangle_train_loader)
+    # torch.save(triangle_train_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/triangle_train_data.pt")
+    # del triangle_train_ds
+    # triangle_val_ds = preprocess_data(triangle_val_loader)
+    # torch.save(triangle_val_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/triangle_val_data.pt")
+    # del triangle_val_ds
+    # print("finish preprocess train/val data")
 
     data = torch.load("square_train_data.pt")
     print(len(data))
 
-    # test data
-    sine_test_ds = CynthiaDataset(metadata_sine_test, sine_test_path)
-    square_test_ds = CynthiaDataset(metadata_square_test, square_test_path)
-    sawtooth_test_ds = CynthiaDataset(metadata_sawtooth_test, sawtooth_test_path)
-    triangle_test_ds = CynthiaDataset(metadata_triangle_test, triangle_test_path)
+    # # test data
+    # sine_test_ds = CynthiaDataset(metadata_sine_test, sine_test_path)
+    # square_test_ds = CynthiaDataset(metadata_square_test, square_test_path)
+    # sawtooth_test_ds = CynthiaDataset(metadata_sawtooth_test, sawtooth_test_path)
+    # triangle_test_ds = CynthiaDataset(metadata_triangle_test, triangle_test_path)
 
-    sine_test_loader = DataLoader(sine_test_ds,  batch_size=len(sine_test_ds)//200, num_workers=200)
-    square_test_loader = DataLoader(square_test_ds,  batch_size=len(square_test_ds)//200, num_workers=200)
-    sawtooth_test_loader = DataLoader(sawtooth_test_ds,  batch_size=len(sawtooth_test_ds)//200, num_workers=200)
-    triangle_test_loader = DataLoader(triangle_test_ds,  batch_size=len(triangle_test_ds)//200, num_workers=200)
+    # sine_test_loader = DataLoader(sine_test_ds,  batch_size=len(sine_test_ds)//num_cpu, num_workers=num_cpu)
+    # square_test_loader = DataLoader(square_test_ds,  batch_size=len(square_test_ds)//num_cpu, num_workers=num_cpu)
+    # sawtooth_test_loader = DataLoader(sawtooth_test_ds,  batch_size=len(sawtooth_test_ds)//num_cpu, num_workers=num_cpu)
+    # triangle_test_loader = DataLoader(triangle_test_ds,  batch_size=len(triangle_test_ds)//num_cpu, num_workers=num_cpu)
 
-    sine_test_ds = preprocess_data(sine_test_loader)
-    square_test_ds = preprocess_data(square_test_loader)
-    sawtooth_test_ds = preprocess_data(sawtooth_test_loader)
-    triangle_test_ds = preprocess_data(triangle_test_loader)
+    # sine_test_ds = preprocess_data(sine_test_loader)
+    # torch.save(sine_test_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/sine_test_data.pt")
+    # del sine_test_ds
+    # square_test_ds = preprocess_data(square_test_loader)
+    # torch.save(square_test_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/square_test_data.pt")
+    # del square_test_ds
+    # sawtooth_test_ds = preprocess_data(sawtooth_test_loader)
+    # torch.save(sawtooth_test_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/sawtooth_test_data.pt")
+    # del sawtooth_test_ds
+    # triangle_test_ds = preprocess_data(triangle_test_loader)
+    # torch.save(triangle_test_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/triangle_test_data.pt")
+    # del triangle_test_ds
 
-    torch.save(sine_test_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/sine_test_data.pt")
-    torch.save(square_test_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/square_test_data.pt")
-    torch.save(sawtooth_test_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/sawtooth_test_data.pt")
-    torch.save(triangle_test_ds, "/rds/general/user/cl222/home/msc_project/sampleCNN/triangle_test_data.pt")
-
-    data = torch.load("sine_test_data.pt")
-    print(len(data))
+    # data = torch.load("sine_test_data.pt")
+    # print(len(data))
